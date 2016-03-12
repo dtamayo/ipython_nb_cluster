d4b3b39d0347108a7e66dc74d620da937843ade9
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-03-12 13:52:31 -0500

    modified checkipnb.py to work with jupyter ipython 4

--- checkipnb.py
+++ checkipnb.py
@@ -11,58 +11,62 @@ import os,sys,time
 
 from Queue import Empty
 
-try:
-    from IPython.kernel import KernelManager
-except ImportError:
-    from IPython.zmq.blockingkernelmanager import BlockingKernelManager as KernelManager
+from jupyter_client import KernelManager
 
-from IPython.nbformat.current import reads, NotebookNode
+from nbformat import read, reads, NotebookNode
 
-def run_notebook(nb):
+def run_notebook(nb, ipynb):
+    max_time = 100 # maximum time for each cell
     km = KernelManager()
     km.start_kernel(stderr=open(os.devnull, 'w'))
-    try:
-        kc = km.client()
-    except AttributeError:
-        # 0.13
-        kc = km
-    kc.start_channels()
-    shell = kc.shell_channel
+    kc = km.client()
     # simple ping:
-    shell.execute("pass")
-    shell.get_msg()
-    
+    kc.execute("pass")
+    kc.get_shell_msg()
+
     cells = 0
     failures = 0
-    for ws in nb.worksheets:
-        for cell in ws.cells:
-            if cell.cell_type != 'code':
-                continue
-            shell.execute(cell.input)
-            # wait for finish, maximum 20s
-            reply = shell.get_msg(timeout=20)['content']
+    for cell in nb.cells:
+        if cell.cell_type != 'code':
+            continue
+        kc.execute(cell.source)
+        try:
+            reply = kc.get_shell_msg(timeout=max_time)['content']
             if reply['status'] == 'error':
                 failures += 1
-                print "\nFAILURE:"
-                print cell.input
-                print '-----'
-                print "raised:"
-                print '\n'.join(reply['traceback'])
-            cells += 1
-            sys.stdout.write('.')
-
-    print
-    print "ran notebook %s" % nb.metadata.name
-    print "    ran %3i cells" % cells
+                print("\n\n************")
+                print("FAILURE:")
+                print("************\n")
+                print(cell.source)
+                print('-----')
+                print("raised:")
+                print('\n'.join(reply['traceback']))
+                print("\n")
+        except Exception:
+            print("\n\n**************************************************")
+            print("Cell timed out. Max time allowed per cell = {0}".format(max_time))
+            print("**************************************************\n")
+            print(cell.source)
+            print("Skipping remaining cells")
+            break
+        cells += 1
+        sys.stdout.write('.')
+   
+    print("\n\n****************************************")
+    print("ran notebook {0}".format(ipynb))
+    print("    ran {0} cells".format(cells))
     if failures:
-        print "    %3i cells raised exceptions" % failures
+        print("    {0} cells raised exceptions".format(failures))
+    print("****************************************")
+    
     kc.stop_channels()
     km.shutdown_kernel()
     del km
 
 if __name__ == '__main__':
     for ipynb in sys.argv[1:]:
-        print "running %s" % ipynb
-        with open(ipynb) as f:
-            nb = reads(f.read(), 'json')
-        run_notebook(nb)
+        print("running {0}".format(ipynb))
+        nb = read(ipynb, as_version=4)
+        #with open(ipynb) as f:
+        #    nb = reads(f.read(), 'json')
+        run_notebook(nb, ipynb)
